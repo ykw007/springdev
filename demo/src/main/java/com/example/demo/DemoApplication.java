c3fbc17afea079097c04802eb07dab4c2a58cc94
ykw007 <ykw007@gmail.com>
2024-07-20 19:53:10 +0900

    Update DemoApplication.java

--- demo/src/main/java/com/example/demo/DemoApplication.java
+++ demo/src/main/java/com/example/demo/DemoApplication.java
@@ -19,103 +19,46 @@ public class DemoApplication {
 
 }
 /*
-import org.apache.commons.net.ftp.FTP;
-import org.apache.commons.net.ftp.FTPClient;
+import java.util.ArrayList;
+import java.util.List;
 
-// ...
+public String generateCronExpression(String intervalType, List<String> detailOptions, String startTime) {
+    String cronExpression = "";
 
-public class FtpDownloadRetryExample {
-
-    public static void main(String[] args) {
-        String server = "ftp.example.com";
-        int port = 21;
-        String username = "your-ftp-username";
-        String password = "your-ftp-password";
-        String remoteFilePath = "/path/on/ftp/server/file.txt";
-        String localFilePath = "path/to/local/file.txt";
-
-        FTPClient ftpClient = new FTPClient();
-        try {
-            ftpClient.connect(server, port);
-            ftpClient.login(username, password);
-            ftpClient.setFileType(FTP.BINARY_FILE_TYPE);
-
-            // 파일 다운로드 시도 (3회까지 재시도)
-            int maxRetries = 3;
-            int retryCount = 0;
-            boolean success = false;
-            while (!success && retryCount < maxRetries) {
-                try {
-                    success = ftpClient.retrieveFile(remoteFilePath, new FileOutputStream(localFilePath));
-                } catch (IOException e) {
-                    System.err.println("다운로드 실패: " + e.getMessage());
-                    retryCount++;
-                    Thread.sleep(1000); // 1초 대기 후 재시도
-                }
-            }
-
-            if (success) {
-                System.out.println("파일 다운로드 성공!");
-            } else {
-                System.out.println("파일 다운로드 실패 (재시도 횟수 초과)");
-            }
-
-            ftpClient.logout();
-        } catch (IOException | InterruptedException e) {
-            e.printStackTrace();
-        } finally {
-            try {
-                ftpClient.disconnect();
-            } catch (IOException e) {
-                e.printStackTrace();
+    if (intervalType.equals("At Once")) {
+        String[] time = startTime.split(":");
+        cronExpression = time[1] + " " + time[0] + " * * *";
+    } else if (intervalType.equals("Daily")) {
+        String[] time = startTime.split(":");
+        cronExpression = time[1] + " " + time[0] + " * * *";
+    } else if (intervalType.equals("Weekly")) {
+        String[] time = startTime.split(":");
+        List<String> daysOfWeek = new ArrayList<>();
+        for (String day : detailOptions) {
+            switch (day) {
+                case "SUN":
+                case "MON":
+                case "TUE":
+                case "WED":
+                case "THU":
+                case "FRI":
+                case "SAT":
+                    daysOfWeek.add(day);
+                    break;
+                default:
+                    // Ignore invalid day selection
             }
         }
+
+        String daysExpression = String.join(",", daysOfWeek);
+        cronExpression = time[1] + " " + time[0] + " * * " + daysExpression;
+    } else if (intervalType.equals("Monthly")) {
+        String[] time = startTime.split(":");
+        String dayOfMonth = detailOptions.get(0).equals("End of Month") ? "L" : "1";
+        cronExpression = time[1] + " " + time[0] + " " + dayOfMonth + " * *";
     }
-}
 
-import org.apache.commons.net.ftp.FTP;
-import org.apache.commons.net.ftp.FTPClient;
-
-import java.io.FileOutputStream;
-import java.io.IOException;
-
-public class FtpDownloadWithTimeoutExample {
-
-    public static void main(String[] args) {
-        String server = "ftp.example.com";
-        int port = 21;
-        String username = "your-ftp-username";
-        String password = "your-ftp-password";
-        String remoteFilePath = "/path/on/ftp/server/file.txt";
-        String localFilePath = "path/to/local/file.txt";
-
-        FTPClient ftpClient = new FTPClient();
-        try {
-            ftpClient.connect(server, port);
-            ftpClient.login(username, password);
-            ftpClient.setFileType(FTP.BINARY_FILE_TYPE);
-
-            // 데이터 전송 시간 초과 설정 (10초)
-            ftpClient.setDataTimeout(10000);
-
-            boolean success = ftpClient.retrieveFile(remoteFilePath, new FileOutputStream(localFilePath));
-            if (success) {
-                System.out.println("파일 다운로드 성공!");
-            } else {
-                System.out.println("파일 다운로드 실패!");
-            }
-
-            ftpClient.logout();
-        } catch (IOException e) {
-            e.printStackTrace();
-        } finally {
-            try {
-                ftpClient.disconnect();
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-        }
-    }
+    return cronExpression;
 }
 
 */
