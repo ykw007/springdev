fc250c8721d881c9e37843c68cb8fbdc760eba4c
ykw007 <ykw007@gmail.com>
2024-07-21 19:16:20 +0900

    Update DemoApplication.java

--- demo/src/main/java/com/example/demo/DemoApplication.java
+++ demo/src/main/java/com/example/demo/DemoApplication.java
@@ -83,4 +83,58 @@ public class NextExecutionTime {
     }
 }
 
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import java.nio.channels.*;
+import java.io.*;
+
+@SpringBootApplication
+public class Application {
+    private static File f;
+    private static FileChannel channel;
+    private static FileLock lock;
+
+    public static void main(String[] args) {
+        try {
+            f = new File("RingOnRequest.lock");
+            if (f.exists()) {
+                f.delete();
+            }
+            channel = new RandomAccessFile(f, "rw").getChannel();
+            lock = channel.tryLock();
+            if(lock == null) {
+                channel.close();
+                throw new RuntimeException("Only 1 instance of MyApp can run.");
+            }
+            ShutdownHook shutdownHook = new ShutdownHook();
+            Runtime.getRuntime().addShutdownHook(shutdownHook);
+
+            SpringApplication.run(Application.class, args);
+            System.out.println("Running");
+
+        } catch(IOException e) {
+            throw new RuntimeException("Could not start process.", e);
+        }
+    }
+
+    public static void unlockFile() {
+        try {
+            if(lock != null) {
+                lock.release();
+                channel.close();
+                f.delete();
+            }
+        } catch(IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    static class ShutdownHook extends Thread {
+        public void run() {
+            unlockFile();
+        }
+    }
+}
+
 */
